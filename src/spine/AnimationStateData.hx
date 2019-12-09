e0a78ecabf8856257a117698765989e5eb25355e
Dan Korostelev <nadako@gmail.com>
2019-12-09 21:12:42 +0100

    clean up AnimationStateData

--- src/spine/AnimationStateData.hx
+++ src/spine/AnimationStateData.hx
@@ -1,14 +1,14 @@
 package spine;
 
-/** Stores mix (crossfade) durations to be applied when {@link AnimationState} animations are changed. */
+/** Stores mix (crossfade) durations to be applied when `AnimationState` animations are changed. */
 class AnimationStateData {
 	/** The SkeletonData to look up animations when they are specified by name. */
-	public var skeletonData:SkeletonData;
-
-	public var animationToMixTime = new Map<String, Float>();
+	public final skeletonData:SkeletonData;
 
 	/** The mix duration to use when no mix duration has been defined between two animations. */
 	public var defaultMix = 0.0;
+
+	final animationToMixTime = new Map<String, Float>();
 
 	public function new(skeletonData:SkeletonData) {
 		if (skeletonData == null)
@@ -18,34 +18,34 @@ class AnimationStateData {
 
 	/** Sets a mix duration by animation name.
 	 *
-	 * See {@link #setMixWith()}. */
+	 * @see `setMixWith`. */
 	public function setMix(fromName:String, toName:String, duration:Float) {
-		var from = this.skeletonData.findAnimation(fromName);
+		var from = skeletonData.findAnimation(fromName);
 		if (from == null)
 			throw new Error("Animation not found: " + fromName);
-		var to = this.skeletonData.findAnimation(toName);
+		var to = skeletonData.findAnimation(toName);
 		if (to == null)
 			throw new Error("Animation not found: " + toName);
-		this.setMixWith(from, to, duration);
+		setMixWith(from, to, duration);
 	}
 
 	/** Sets the mix duration when changing from the specified animation to the other.
 	 *
-	 * See {@link TrackEntry#mixDuration}. */
+	 * See `TrackEntry.mixDuration`. */
 	public function setMixWith(from:Animation, to:Animation, duration:Float) {
 		if (from == null)
 			throw new Error("from cannot be null.");
 		if (to == null)
 			throw new Error("to cannot be null.");
 		var key = from.name + "." + to.name;
-		this.animationToMixTime[key] = duration;
+		animationToMixTime[key] = duration;
 	}
 
 	/** Returns the mix duration to use when changing from the specified animation to the other, or the {@link #defaultMix} if
 	 * no mix duration has been set. */
 	public function getMix(from:Animation, to:Animation):Float {
 		var key = from.name + "." + to.name;
-		var value = this.animationToMixTime[key];
-		return value == null ? this.defaultMix : value;
+		var value = animationToMixTime[key];
+		return value == null ? defaultMix : value;
 	}
 }
